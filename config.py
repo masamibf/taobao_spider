3205b856108380efb012e54939a76073f916b0d4
长泽雅美男友 <31460716+masamibf@users.noreply.github.com>
2018-01-16 20:40:33 +0800

    加入py文件

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@author: @长泽雅美男友
+@contact: 374454765@qq.com 
+@file: config.py 
+@time: 2018/1/15 18:35 
+"""
+
+MONGO_URL = 'localhost'
+MONGO_DB = 'taobao'
+MONGO_TABLE = 'product'
+
+SERVICE_ARGS = ['--load-images=false','--disk-cache=true']
